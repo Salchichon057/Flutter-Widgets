7aac24d6c4917553c1e55b8e99a94d0755bd497d
steve <stev8855@gmail.com>
2024-05-15 17:02:03 -0500

    add card type 4

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -36,6 +36,8 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cards.map((card) => _CardType1(label: card['label'],elevation: card['elevation'])),
           ...cards.map((card) => _CardType2(label: card['label'],elevation: card['elevation'])),
+          ...cards.map((card) => _CardType3(label: card['label'],elevation: card['elevation'])),
+          ...cards.map((card) => _CardType4(label: card['label'],elevation: card['elevation'])),
 
           const SizedBox(height: 50),
         ],
@@ -116,6 +118,88 @@ class _CardType2 extends StatelessWidget {
             )
           ]
         )
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  
+  const _CardType3({
+    required this.label,
+    required this.elevation
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceContainerHighest,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled', style: const TextStyle(fontSize: 18))
+            )
+          ]
+        )
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  
+  const _CardType4({
+    required this.label,
+    required this.elevation
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      clipBehavior: Clip.hardEdge, // Evita que los hijos se salgan del borde del Card (padre)
+      elevation: elevation,
+      child: Stack(
+        // Diferencia entre Stack y Column es que Stack permite colocar los elementos uno encima del otro
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20))
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            )
+          )
+        ]
       ),
     );
   }
