579f13f4a80f96080fb0e300b142adc63e8319f7
steve <stev8855@gmail.com>
2024-05-15 16:50:16 -0500

    add cards type 2

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -5,7 +5,8 @@ const cards = <Map<String, dynamic>>[
   { 'elevation': 1.0, 'label': 'Elevation 1'},
   { 'elevation': 2.0, 'label': 'Elevation 2'},
   { 'elevation': 3.0, 'label': 'Elevation 3'},
-  { 'elevation': 4.0, 'label': 'Elevation 4'}
+  { 'elevation': 4.0, 'label': 'Elevation 4'},
+  { 'elevation': 5.0, 'label': 'Elevation 5'},
 ];
 
 class CardsScreen extends StatelessWidget {
@@ -34,7 +35,9 @@ class _CardsView extends StatelessWidget {
       child: Column(
         children: [
           ...cards.map((card) => _CardType1(label: card['label'],elevation: card['elevation'])),
-          // ...cards.map((card) => _CardType2(label: card['label'],elevation: card['elevation'])),
+          ...cards.map((card) => _CardType2(label: card['label'],elevation: card['elevation'])),
+
+          const SizedBox(height: 50),
         ],
       ),
     );
@@ -76,3 +79,44 @@ class _CardType1 extends StatelessWidget {
   }
 }
 
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  
+  const _CardType2({
+    required this.label,
+    required this.elevation
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        side: BorderSide(color: colors.outline),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline', style: const TextStyle(fontSize: 18))
+            )
+          ]
+        )
+      ),
+    );
+  }
+}
