d6b9c7fa59b354a2a15be3deb2a812a7f031b2a4
steve <stev8855@gmail.com>
2024-05-15 11:57:56 -0500

    apptheme created

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.purple,
+  Colors.orange,
+  Colors.teal,
+  Colors.pink,
+  Colors.amber,
+  Colors.brown,
+  Colors.cyan,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+    }) : assert(selectedColor >= 0 , 'selectedColor must be greater than or equal to 0'),
+        assert(selectedColor < colorList.length,
+          'selectedColor must be less than ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    )
+  );
+}
