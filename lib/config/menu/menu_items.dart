57a760f567a5ec60d652a99f18d8a49d40e3f1e3
steve <stev8855@gmail.com>
2024-05-15 12:15:17 -0500

    refactor menu_items

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class MenItem {
+class MenuItem {
   final String title;
   final String subtitle;
   final String link;
   final IconData icon;
 
-  const MenItem({
+  const MenuItem({
     required this.title,
     required this.subtitle,
     required this.link,
@@ -14,14 +14,14 @@ class MenItem {
   });
 }
 
-const AppMenuItems = <MenItem>[
-  MenItem(
+const appMenuItems = <MenuItem>[
+  MenuItem(
     title: 'Botones',
     subtitle: 'Varios botones en Flutter',
     link: '/buttons',
     icon: Icons.smart_button_outlined
   ),
-  MenItem(
+  MenuItem(
     title: 'Tarjetas',
     subtitle: 'Un contenedor estilizado',
     link: '/card',
